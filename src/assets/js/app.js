14e98d2e88320b17facf8af7bb93c6c155975230
Kyle Cuthbertson <UP687318@myport.ac.uk>
2017-07-19 12:47:44 +0100

    Added Functions

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -9,4 +9,67 @@ import Foundation from 'foundation-sites';
 //import './lib/foundation-explicit-pieces';
 
 
+
+
+// Contact Form JS
+// Variables
+const resetBtn = document.getElementById('resetButton');
+const submitBtn = document.getElementById('sendButton');
+
+const alertField = document.getElementById('alert-fields');
+
+const nameField = document.getElementById('name');
+const emailField = document.getElementById('email');
+const numberField = document.getElementById('number');
+const textField = document.getElementById('textarea');
+
+
+// Submit Butto
+submitBtn.addEventListener("click", () => {
+  if (nameField.value === '') {
+    nameField.style.border = '2px red solid';
+    alertField.style.display = 'block';
+  }
+
+  if (emailField.value === '') {
+    emailField.style.border = '2px red solid';
+  }
+
+  if (textField.value === '') {
+    textField.style.border = '2px red solid';
+  }
+})
+
+nameField.addEventListener("keydown", () => {
+  nameField.style.border = '1px grey solid';
+})
+
+emailField.addEventListener("keydown", () => {
+  emailField.style.border = '1px grey solid';
+})
+
+textField.addEventListener("keydown", () => {
+  textField.style.border = '1px grey solid';
+})
+
+// Reset Button
+
+resetBtn.addEventListener("click", () => {
+  nameField.value = '';
+  emailField.value = '';
+  numberField.value = '';
+  textField.value = '';
+})
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).foundation();
